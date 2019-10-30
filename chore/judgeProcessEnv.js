f39c8b3a44f0986a6730dd44cb4cff710cc3c0b7
cycjimmy <cycjimmy@gmail.com>
2019-10-30 17:00:51 +0800

    feat: add function for judge process env

---
+++ chore/judgeProcessEnv.js
@@ -0,0 +1,9 @@
+/**
+ * Judge Process Env
+ * @type {{IS_DEPLOYMENT: boolean, IS_PRODUCTION: boolean, IS_DEVELOPMENT: boolean}}
+ */
+module.exports = ({
+  IS_DEVELOPMENT: process.env.NODE_ENV === 'development',
+  IS_PRODUCTION: process.env.NODE_ENV === 'production',
+  IS_DEPLOYMENT: process.env.NODE_ENV === 'deployment',
+});
